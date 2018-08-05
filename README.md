eda011e08560b7fc880e179d040083a9c59638d3
Fatima Sheremetyeva <fsheremetyeva@gmail.com>
2018-08-05 14:35:38 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+
+This repository contains config files for installing Vagrant as well as MVC framework for building dynamic pages.
--- myframework/views/welcome.php
+++ myframework/views/welcome.php
@@ -1,2 +1,6 @@
     <div class="container">
+
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel odio accumsan, accumsan dolor quis, consequat sem. Aenean pulvinar arcu luctus rutrum mattis. Etiam neque metus, pretium et erat in, sollicitudin elementum velit. Morbi at lacus augue. Integer ultricies vestibulum nulla, laoreet bibendum metus mattis sit amet. Pellentesque est metus, dictum et lorem quis, interdum rutrum ex. Proin augue nisl, viverra feugiat augue et, fermentum porttitor elit. Ut sit amet ligula quis elit vehicula facilisis vel non arcu. Aliquam auctor lorem id ligula lobortis mollis. Vivamus tristique ut justo convallis vestibulum. Proin vehicula tortor rhoncus, hendrerit urna non, feugiat urna.
+      </p>
+
     </div>
