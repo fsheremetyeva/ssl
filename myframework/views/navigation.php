1a9bcb5840e942f7daeafbc58236559b7de58a15
Fatima Sheremetyeva <fsheremetyeva@gmail.com>
2018-08-12 15:52:25 -0500

    Added Bootstrap brand logo back to keep responsive navbar

--- myframework/views/navigation.php
+++ myframework/views/navigation.php
@@ -2,7 +2,10 @@
 
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container">
-
+      <a class="navbar-brand" href="/welcome">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
 
